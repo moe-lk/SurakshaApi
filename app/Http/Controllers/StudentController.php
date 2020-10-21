5eda6930879ecbf2637545a10729cb58007324f4
Sennen Randika <randikasennen@gmail.com>
2020-10-22 00:33:17 +0530

    Updated StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,24 +8,27 @@ class StudentController extends Controller
 {
     /**
      * @SWG\Get(
-     *   path="/api/search-student",
-     *   tags={"admins"},
-     *   summary="Get student details for the claim",
-     *   operationId="searchStudent",
-     *   @SWG\Response(response=200, description="Student found"),
-     *   @SWG\Response(response=404, description="Student not found"),
-     *   @SWG\Response(response=406, description="Invalid NSID"),
-     *   @SWG\Response(response=500, description="Internal server error"),
-	 *		@SWG\Parameter(
-     *          in="path",
-     *          name="nsid",
-     *          required=true, 
-     *          type="string" 
-     *      ),
+     *     path="/api/student/{nsid}",
+     *     summary="Search student by NSID",
+     *     description="Returns a single student for the provided NSID",
+     *     operationId="getStudentByNSID",
+     *     tags={"student"},
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         description="NSID of the student to return",
+     *         in="path",
+     *         name="nsid",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Response(response=200, description="Student found"),
+     *     @SWG\Response(response=400, description="Invalid NSID provided"),
+     *     @SWG\Response(response=404, description="Student not found"),
+     *     @SWG\Response(response=500, description="Internal server error"),
+     *     security={{"api_key": {}}}
      * )
-     *
      */
-    public function searchStudent(Request $request){
-        //
+    public function getStudentByNSID(){
+    
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/search-student', 'StudentController@searchStudent');
+Route::get('/student/{nsid}', 'StudentController@getStudentByNSID');
