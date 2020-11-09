e8f189039ca1c8352c900ecd950221fcb525949c
Sennen Randika <randikasennen@gmail.com>
2020-11-09 22:07:12 +0530

    Updated the table with necessary modifications

--- database/migrations/2020_11_06_095852_student_insurance_claims.php
+++ database/migrations/2020_11_06_095852_student_insurance_claims.php
@@ -14,30 +14,32 @@ class StudentInsuranceClaims extends Migration
     public function up()
     {
         Schema::create('student_insurance_claims', function (Blueprint $table) {
-            $table->string('claim_id');
-            $table->string('nsid');
-            $table->string('student_name_with_initials');
-            $table->string('student_full_name');
+            $table->string('claim_id', 20);
+            $table->string('nsid', 12);
+            $table->string('student_name_with_initials', 100);
+            $table->string('student_full_name', 255);
             $table->date('student_date_of_birth');
-            $table->string('mothers_name');
-            $table->string('mothers_nic');
-            $table->string('fathers_name');
-            $table->string('fathers_nic');
-            $table->string('legal_guardians_name');
+            $table->string('mothers_name', 255);
+            $table->string('mothers_nic', 12);
+            $table->string('fathers_name', 255);
+            $table->string('fathers_nic', 12);
+            $table->string('legal_guardians_name', 255);
             $table->date('incident_date');
             $table->date('claim_requested_date');
             $table->date('claim_entered_date');
-            $table->string('claim_type');
+            $table->string('claim_type', 50);
             $table->text('claim_description');
-            $table->decimal('claim_amount_requested');
-            $table->decimal('claim_amount_paid');
-            $table->string('payment_receivers_name');
-            $table->string('payment_receivers_nic');
+            $table->decimal('claim_amount_requested', 10, 2);
+            $table->decimal('claim_amount_paid', 10, 2);
+            $table->string('payment_receivers_name', 255);
+            $table->string('payment_receivers_nic', 12);
             $table->text('payee_address');
             $table->integer('school_census_id');
-            $table->string('school_name');
-            $table->string('admission_id');
+            $table->string('school_name', 255);
+            $table->string('admission_id', 12);
 
+            $table->unique('claim_id');
+            $table->primary(['claim_id', 'nsid']);	
         });
     }
 
